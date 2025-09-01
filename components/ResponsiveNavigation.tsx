ff73dcaa93077a063d7c675173a03d40603aebad
dryanez <dr.felipeyanez@gmail.com>
2025-09-02 01:03:34 +0200

    bigger

--- components/ResponsiveNavigation.tsx
+++ components/ResponsiveNavigation.tsx
@@ -36,9 +36,9 @@ export default function ResponsiveNavigation({
         {/* Mobile Menu Button and Logo */}
         <div className="flex items-center md:hidden">
           <button onClick={() => setMenuOpen(!menuOpen)} className="p-1 rounded-md bg-[#FE6448] mr-2">
-            <svg className="w-6 h-6 text-white" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M4 6h16M4 12h16m-7 6h7" /></svg>
+            <svg className="w-8 h-8 text-white" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M4 6h16M4 12h16m-7 6h7" /></svg>
           </button>
-          <img src={logoSrc} alt={logoAlt} className="w-16 h-16 object-contain" />
+          <img src={logoSrc} alt={logoAlt} className="w-20 h-20 object-contain" />
         </div>
         {/* Desktop Logo - Centered */}
         <div className="hidden md:flex ml-auto">
